90be8855763eeec2d1ceeef08c36029d47c39f10
Alan Mendelevich <ailon@ailon.org>
2025-07-29 15:24:21 +0300

    add arrow marker creation

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,17 @@
-import { AnnotationState, MarkerArea } from "@markerjs/react-native-markerjs";
-import { useState } from "react";
-import { View } from "react-native";
+import {
+  AnnotationState,
+  MarkerArea,
+  MarkerAreaHandle,
+} from "@markerjs/react-native-markerjs";
+import { useRef, useState } from "react";
+import { Button, View } from "react-native";
 
 const targetImage = require("@/assets/images/sample-image.png");
 
 export default function Index() {
   const [annotation, setAnnotation] = useState<AnnotationState | null>(null);
+
+  const markerAreaRef = useRef<MarkerAreaHandle>(null);
 
   return (
     <View
@@ -18,8 +24,18 @@ export default function Index() {
       <MarkerArea
         targetSrc={targetImage}
         annotation={annotation}
+        ref={markerAreaRef}
         onAnnotationChange={setAnnotation}
       />
+
+      <View style={{ flex: 1 / 4 }}>
+        <Button
+          title="Add arrow"
+          onPress={() => {
+            markerAreaRef.current?.createMarker("ArrowMarker");
+          }}
+        />
+      </View>
     </View>
   );
 }
